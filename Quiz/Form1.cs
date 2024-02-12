cafe77ae4c7442d95fe371897ab46a3764e6e00f
Marcel <marcel.arnold@stud.hs-emden-leer.de>
2024-02-12 18:03:16 +0100

    Projektdateien hinzufügen.

---
+++ Quiz/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        public static Form1 instance;
+        string ColorTheme = "Pink";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void On_ButtonA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void On_ButtonB_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void On_ButtonC_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void On_ButtonD_Click(object sender, EventArgs e)
+        {
+                                
+        }
+    }
+}
