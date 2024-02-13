26eed2a5d45747666291470b087d6e2f196af645
Marcel <marcel.arnold@stud.hs-emden-leer.de>
2024-02-13 16:12:33 +0100

    changes

--- Quiz/Form1.Designer.cs
+++ Quiz/Form1.Designer.cs
@@ -28,69 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.buttonA = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.buttonD = new System.Windows.Forms.Button();
+            this.lblFrage = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblScore = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonC
             // 
-            this.button1.BackColor = System.Drawing.Color.Gray;
-            this.button1.Location = new System.Drawing.Point(149, 330);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 55);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "C";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.On_ButtonB_Click);
+            this.buttonC.BackColor = System.Drawing.Color.White;
+            this.buttonC.Font = new System.Drawing.Font("Tw Cen MT", 20.25F, System.Drawing.FontStyle.Bold);
+            this.buttonC.Location = new System.Drawing.Point(149, 330);
+            this.buttonC.Name = "buttonC";
+            this.buttonC.Size = new System.Drawing.Size(155, 55);
+            this.buttonC.TabIndex = 0;
+            this.buttonC.Text = "C";
+            this.buttonC.UseVisualStyleBackColor = false;
+            this.buttonC.Click += new System.EventHandler(this.On_ButtonC_Click);
             // 
-            // button2
+            // buttonA
             // 
-            this.button2.BackColor = System.Drawing.Color.Gray;
-            this.button2.Location = new System.Drawing.Point(149, 215);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(155, 60);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "A";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.On_ButtonA_Click);
+            this.buttonA.BackColor = System.Drawing.Color.White;
+            this.buttonA.Font = new System.Drawing.Font("Tw Cen MT", 20.25F, System.Drawing.FontStyle.Bold);
+            this.buttonA.Location = new System.Drawing.Point(149, 215);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(155, 60);
+            this.buttonA.TabIndex = 1;
+            this.buttonA.Text = "A";
+            this.buttonA.UseVisualStyleBackColor = false;
+            this.buttonA.Click += new System.EventHandler(this.On_ButtonA_Click);
             // 
-            // button3
+            // buttonB
             // 
-            this.button3.BackColor = System.Drawing.Color.Gray;
-            this.button3.Location = new System.Drawing.Point(423, 215);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(155, 60);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "B";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.On_ButtonC_Click);
+            this.buttonB.BackColor = System.Drawing.Color.White;
+            this.buttonB.Font = new System.Drawing.Font("Tw Cen MT", 20.25F, System.Drawing.FontStyle.Bold);
+            this.buttonB.Location = new System.Drawing.Point(423, 215);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(155, 60);
+            this.buttonB.TabIndex = 2;
+            this.buttonB.Text = "B";
+            this.buttonB.UseVisualStyleBackColor = false;
+            this.buttonB.Click += new System.EventHandler(this.On_ButtonB_Click);
             // 
-            // button4
+            // buttonD
             // 
-            this.button4.BackColor = System.Drawing.Color.Gray;
-            this.button4.Location = new System.Drawing.Point(423, 330);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(155, 55);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "D";
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.On_ButtonD_Click);
+            this.buttonD.BackColor = System.Drawing.Color.White;
+            this.buttonD.Font = new System.Drawing.Font("Tw Cen MT", 20.25F, System.Drawing.FontStyle.Bold);
+            this.buttonD.Location = new System.Drawing.Point(423, 330);
+            this.buttonD.Name = "buttonD";
+            this.buttonD.Size = new System.Drawing.Size(155, 55);
+            this.buttonD.TabIndex = 3;
+            this.buttonD.Text = "D";
+            this.buttonD.UseVisualStyleBackColor = false;
+            this.buttonD.Click += new System.EventHandler(this.On_ButtonD_Click);
             // 
-            // label1
+            // lblFrage
             // 
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Tw Cen MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(194, 129);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(351, 55);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Frage";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblFrage.BackColor = System.Drawing.Color.Transparent;
+            this.lblFrage.Font = new System.Drawing.Font("Tw Cen MT", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrage.ForeColor = System.Drawing.Color.Black;
+            this.lblFrage.Location = new System.Drawing.Point(12, 126);
+            this.lblFrage.Name = "lblFrage";
+            this.lblFrage.Size = new System.Drawing.Size(700, 55);
+            this.lblFrage.TabIndex = 4;
+            this.lblFrage.Text = "Frage";
+            this.lblFrage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.Color.Transparent;
+            this.lblResult.Font = new System.Drawing.Font("Tw Cen MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.Black;
+            this.lblResult.Location = new System.Drawing.Point(265, 181);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(225, 31);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Wähle eine Antwort aus";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblScore
+            // 
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Tw Cen MT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.Color.Black;
+            this.lblScore.Location = new System.Drawing.Point(601, 450);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(95, 31);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.Text = "Score";
+            this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
@@ -98,24 +134,29 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(724, 504);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblFrage);
+            this.Controls.Add(this.buttonD);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.buttonA);
+            this.Controls.Add(this.buttonC);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Quiz";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button buttonB;
+        private System.Windows.Forms.Button buttonD;
+        private System.Windows.Forms.Label lblFrage;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblScore;
+        public System.Windows.Forms.Button buttonA;
     }
 }
 
--- Quiz/Form1.cs
+++ Quiz/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,31 +14,217 @@ namespace Quiz
 {
     public partial class Form1 : Form
     {
-        public static Form1 instance;
-        string ColorTheme = "Pink";
+        int CorrectA = 0;
+        int QuestionID = 0;
+        int Score = 0;
+        string PromptMessage = "Select the answer!";
+        bool GaveA = false;
+
+
         public Form1()
         {
             InitializeComponent();
+            PromptMessage = "Select the answer!";
+
+            ManageQuiz();
         }
 
         private void On_ButtonA_Click(object sender, EventArgs e)
         {
+            if (CorrectA == 1)
+            {
+                lblResult.Text = "Correct choice!";
+                buttonA.Enabled = false;
+                buttonB.Enabled = false;
+                buttonD.Enabled = false;
+                buttonC.Enabled = false;
+                timer1.Start();
+                
+                
 
+                if (GaveA == false)
+                {
+                    Score++;
+                }
+
+                //QuestionID++;
+            }
+
+            else
+            {
+                lblResult.Text = "Incorrect choice!";
+            }
+
+            if (GaveA == false)
+            {
+                GaveA = true;
+            }
+
+
+            //ManageQuiz();
         }
 
         private void On_ButtonB_Click(object sender, EventArgs e)
         {
+            if (CorrectA == 2)
+            {
+                lblResult.Text = "Correct choice!";
+                buttonC.Enabled = false;
+                buttonB.Enabled = false;
+                buttonD.Enabled = false;
+                buttonA.Enabled = false;
+                timer1.Start();
+                
+
+
+                if (GaveA == false)
+                {
+                    Score++;
+                }
+                
+                //QuestionID++;
+            }
+
+            else
+            {
+                lblResult.Text = "Incorrect choice!";
+            }
+
+            if (GaveA == false)
+            {
+                GaveA = true;
+            }
+
+
+            //ManageQuiz();
 
         }
 
         private void On_ButtonC_Click(object sender, EventArgs e)
         {
+            if (CorrectA == 3)
+            {
+                lblResult.Text = "Correct choice!";
+                buttonA.Enabled = false;
+                buttonC.Enabled = false;
+                buttonD.Enabled = false;
+                buttonB.Enabled = false;
+                timer1.Start();
+                
+
+
+                if (GaveA == false)
+                {
+                    Score++;
+                }
+
+                //QuestionID++;
+            }
+
+            else
+            {
+                lblResult.Text = "Incorrect choice!";
+            }
+
+            if (GaveA == false)
+            {
+                GaveA = true;
+            }
+
+
+            //ManageQuiz();
 
         }
 
         private void On_ButtonD_Click(object sender, EventArgs e)
         {
-                                
+            if (CorrectA == 4)
+            {
+                lblResult.Text = "Correct choice!";
+                buttonA.Enabled = false;
+                buttonB.Enabled = false;
+                buttonC.Enabled = false;
+                buttonD.Enabled = false;
+                timer1.Start();
+
+
+                if (GaveA == false)
+                {
+                    Score++;
+                }
+
+                //QuestionID++;   
+            }
+
+            else
+            {
+                lblResult.Text = "Incorrect choice!";
+            }
+
+            if (GaveA == false)
+            {
+                GaveA = true;
+            }
+
+
+           //ManageQuiz();
+
+        }
+
+
+        private void ManageQuiz()
+        {
+            timer1.Stop();
+            QuestionID++;
+            lblScore.Text = "Score: " + Score;
+
+            Question(1, "Writes Hello World!", "Gives a error: Hello World!", "Writes Hello world", "Plays a mp3: Hello world", 1, "Console.WriteLine(\"Hello World!\")");
+            Question(2, "Writes Hello World", "Gives a error!", "Writes Hello world", "Gives you free bitcoin", 2, "Console.WriteLine(Hello World!)");
+            Question(3, "Writes 4", "Gives a error!", "Writes 2 + 2", "Gives you free bitcoin", 3, "Console.WriteLine(\"2 + 2\")");
+
+            if (QuestionID == 4)
+            {
+                //End of the Quiz
+                lblFrage.Text = "End of the Quiz!";
+                lblResult.Text = "Score: " + Score;
+                buttonC.Enabled = false;
+                buttonA.Enabled = false;
+                buttonB.Enabled = false;
+                buttonD.Enabled = false;
+            }
+        }
+
+
+        public void Question(int QuestionNumber, string A1, string A2, string A3, string A4, int CorrectBtn, string Frage)
+        {
+            if (QuestionNumber == QuestionID)
+            {
+                GaveA = false;
+
+                buttonA.Text = A1;
+                buttonB.Text = A2;
+                buttonC.Text = A3;
+                buttonD.Text = A4;
+                lblFrage.Text = Frage;
+
+                CorrectA = CorrectBtn;
+
+                buttonC.Enabled = true;
+                buttonA.Enabled = true;
+                buttonB.Enabled = true;
+                buttonD.Enabled = true;
+
+                lblResult.Text = PromptMessage;
+
+                  
+            }
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ManageQuiz();
         }
     }
 }
